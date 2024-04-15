f6130d9ee57ddd9d42280785b0b2bcd5cf7f3393
Theo Olsson <theo1022@hotmail.se>
2024-04-15 15:42:48 +0200

    Minor edits to IsTollFreeDate() changed return type to be in line with other return types, and made minor edits to nestled if statement based on personal preference

--- TollCalculator.cs
+++ TollCalculator.cs
@@ -81,7 +81,7 @@ public class TollCalculator
         return 0;
     }
 
-    private Boolean IsTollFreeDate(DateTime date)
+    private bool IsTollFreeDate(DateTime date)
     {
         int year = date.Year;
         int month = date.Month;
@@ -91,17 +91,15 @@ public class TollCalculator
 
         if (year == 2013)
         {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
+            if ((month == 1 && day == 1) ||
+                (month == 3 && (day == 28 || day == 29)) ||
+                (month == 4 && (day == 1 || day == 30)) ||
+                (month == 5 && (day == 1 || day == 8 || day == 9)) ||
+                (month == 6 && (day == 5 || day == 6 || day == 21)) ||
+                (month == 7) ||
+                (month == 11 && day == 1) ||
+                (month == 12 && (day == 24 || day == 25 || day == 26 || day == 31)))
                 return true;
-            }
         }
         return false;
     }
