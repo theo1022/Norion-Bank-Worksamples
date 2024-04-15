f15653f8ea3674067fc27dc0800154a07283f1b4
Theo Olsson <theo1022@hotmail.se>
2024-04-15 15:18:20 +0200

    CalculateTollFee renamed and edited Changed the name of second GetTollFee method and changed the if statements to be easier to read

--- TollCalculator.cs
+++ TollCalculator.cs
@@ -21,8 +21,8 @@ public class TollCalculator
         int totalFee = 0;
         foreach (DateTime date in dates)
         {
-            int nextFee = GetTollFee(date);
-            int tempFee = GetTollFee(intervalStart);
+            int nextFee = CalculateTollFee(date);
+            int tempFee = CalculateTollFee(intervalStart);
 
             long diffInMillies = date.Millisecond - intervalStart.Millisecond;
             long minutes = diffInMillies/1000/60;
@@ -51,23 +51,35 @@ public class TollCalculator
         return false;
     }
 
-    public int GetTollFee(DateTime date)
+    public int CalculateTollFee(DateTime currentPass)
     {
-        if (IsTollFreeDate(date)) return 0;
+        if (IsTollFreeDate(currentPass))) return 0;
 
-        int hour = date.Hour;
-        int minute = date.Minute;
+        var hour = currentPass.Hour;
+        var minute = currentPass.Minute;
 
-        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-        else return 0;
+        if (hour >= 6 && (hour <= 18 && minute < 30))
+        {
+            //06:00–06:29     8 kr
+            if (hour == 6 || minute < 30) return 8;
+            //06:30–06:59     13 kr
+            if (hour < 7) return 13;
+            //07:00–07:59     18 kr
+            if (hour < 8) return 18;
+            //08:00–08:29     13 kr
+            if (hour == 8 && minute < 30) return 13;
+            //08:30–14:59     8 kr
+            if (hour < 15) return 8;
+            //15:00–15:29     13 kr
+            if (hour == 15 && minute < 30) return 13;
+            //15:30–16:59     18 kr
+            if (hour < 17) return 18;
+            //17:00–17:59     13 kr
+            if (hour < 18) return 13;
+            //18:00–18:29     8 kr
+            if (hour == 18 && minute < 30) return 8;
+        }
+        return 0;
     }
 
     private Boolean IsTollFreeDate(DateTime date)
